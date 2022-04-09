57875c1d4c51b35e5d9339048228dc953dd632a1
hyuan01 <79280968+hyuan01@users.noreply.github.com>
2022-04-09 14:16:18 -0400

    Update test_selenium.py

--- test_selenium.py
+++ test_selenium.py
@@ -1,13 +1,13 @@
 import pytest
 
 # name of the fixture function
-@pytest.mark.usefixtures("test")
+@pytest.mark.usefixtures("setup")
 class Test:
     def test_title(self):
         self.driver.get('https://google.com')
         assert self.driver.title == "Google"
 
-    def test_title_blog(self):
+    def test_title2(self):
         self.driver.get('https://youtube.com')
         assert self.driver.title == "YouTube"
 
